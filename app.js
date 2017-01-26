83802447f0350d92505fbd300954448258b9a834
Laura Vingada <laura@vingada.com>
2017-01-25 20:29:48 -0500

    added partials folder and footer and header partials in the folder.

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'The Weather',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome to my home page.',
   });
 });
@@ -16,7 +24,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About The Weather',
-    currentYear: new Date().getFullYear(),
   });
 });
 
