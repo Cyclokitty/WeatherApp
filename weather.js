a57ae9df0975d4e1820d4428f61c2174f9744cc9
Laura Vingada <laura@vingada.com>
2017-02-08 17:46:32 -0500

    added css file

--- weather.js
+++ weather.js
@@ -3,21 +3,20 @@ const request = require('request');
 const getWeather = (lat, lng, callback) => {
   request({
     url: `https://api.darksky.net/forecast/0f48f14a2478df23f2ea57fa481e7904/${lat},${lng}`,
-    json: true
+    json: true,
   }, (error, response, body) => {
       if (!error && response.statusCode === 200) {
         callback(undefined, {
           temperature: body.currently.temperature,
           summary: body.currently.summary,
-          tomorrow: body.hourly.summary
-        })
+          tomorrow: body.hourly.summary,
+        });
       } else {
         console.log(`Unable to fetch weather data.`);
       }
   });
-
-}
+};
 
 module.exports = {
-  getWeather
+  getWeather,
 };
